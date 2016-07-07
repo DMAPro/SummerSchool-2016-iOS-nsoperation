d00d5ec92682e672687bb5d80fa2187f10931fd6
Demid <demid.itsk@gmail.com>
2016-07-07 23:12:09 +0500

    С историей коммитов извините, я переоценил свои силы :) Коммит будет один. Но зато я комментарии добавил.

--- BackgroundImageDownload/ViewController.m
+++ BackgroundImageDownload/ViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "DMAImageTableViewCell.h"
+#import "DMAImageProvider.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @end
 
 @implementation ViewController
+
+const int numberOfCells = 30;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,6 +26,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView methods
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return numberOfCells;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 168;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    DMAImageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ImageCell"];
+    
+    [cell setCustomImage];
+    
+    return cell;
 }
 
 @end
